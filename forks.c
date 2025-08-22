cc91cc2bcafa1219ad515e42b38168d592ccfd06
Nyx <elerii098@gmail.com>
2025-08-22 14:59:33 -0300

    terminado forks

--- forks.c
+++ forks.c
@@ -1,50 +1,44 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<sys/types.h>
+#include<sys/wait.h>
 #include<unistd.h>
 
 int main(int argc, char *argv[])
 {
   pid_t b = fork();
-  if(b == 0){
+  if (b == 0) {
+    printf("B is my name.\nMy PID:%d\nMy PPID:%d\n\n", getpid(), getppid());
     pid_t d = fork();
+    if (d==0) {
+      printf("D is my name.\nMy PID:%d\nMy PPID:%d\n\n", getpid(), getppid());
+      sleep(10);
+      return 0;
+    }
     pid_t e = fork();
+    if (e==0) {
+      printf("E is my name.\nMy PID:%d\nMy PPID:%d\n\n", getpid(), getppid());
+      sleep(10);
+      return 0;
+    }
     pid_t f = fork();
-    if(d == 0){
-      printf("D is my name.\nMy PID:%d\n. My PPID:%d\n\n", getpid(), getppid());
-      while(1){
-        
-      }
-    }else if(e == 0){
-      printf("E is my name.\nMy PID:%d\n. My PPID:%d\n\n", getpid(), getppid());
-      while(1){
-        
-      }
-    }else if(f == 0){
-      printf("F is my name.\nMy PID:%d\n. My PPID:%d\n\n", getpid(), getppid());
-      while(1){
-        
-      }
-    }else{
-      printf("B is my name.\nMy PID:%d\n. My PPID:%d\n\n", getpid(), getppid());
-      while(1){
-        
-      }
+    if (f==0) {
+      printf("F is my name.\nMy PID:%d\nMy PPID:%d\n\n", getpid(), getppid());
+      sleep(10);
+      return 0;
     }
-    
-  }else {
-    pid_t c = fork();
-    if(c == 0){
-      printf("C is my name.\nMy PID:%d\n. My PPID:%d\n\n", getpid(), getppid());
-      while(1){
-        
-      }
-    }else {
-      printf("A is my name.\nMy PID:%d\n. My PPID:%d\n\n", getpid(), getppid());
-      while(1){
-        
-      }
-    }
+    wait(NULL);
+    return 0;
   }
 
+  pid_t c = fork();
+  if(c==0){
+    printf("C is my name.\nMy PID:%d\nMy PPID:%d\n\n", getpid(), getppid());
+    sleep(10);
+    return 0;
+  }
+  printf("A is my name.\nMy PID:%d\nMy PPID:%d\n\n", getpid(), getppid());
+  wait(NULL);
   return 0;
+
 }
